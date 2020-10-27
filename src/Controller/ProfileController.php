dc29950b38779565752799525ea7e611785b6e6e
dorianerousseau <contact.dorianerousseau@gmail.com>
2020-10-27 17:16:18 +0100

    mise en place du theme

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class ProfileController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class UserController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+
+}
