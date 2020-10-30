fd63cf012c51592ce07867ef5da86327e36a0195
dorianerousseau <contact.dorianerousseau@gmail.com>
2020-10-30 17:33:05 +0100

    mise a jour
    - profil
    - theme
    - user
    - default (homepage)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,9 +10,12 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 
 class UserController extends AbstractController
@@ -22,33 +25,20 @@ class UserController extends AbstractController
      * @Route("/membre/inscription", name="user_create", methods={"GET|POST"})
      *
      */
-    public function create_user(Request $request, UserPasswordEncoderInterface $encoder)
+    public function create_user(Request $request, UserPasswordEncoderInterface $encoder, SluggerInterface $slugger)
     {
-        # 1. Création d'un objet user
+        # 1. Création d'un nouvel utilisateur
         $user = new User();
         $user->setRoles(['ROLE_USER']);
 
-        # 2. Création du Formulaire
+        # 2. Création du Formulaire d'inscription
         $form = $this->createFormBuilder($user)
             ->add('pseudo', TextType::class)
             ->add('age', TextType::class)
             ->add('sex', ChoiceType::class, [
-                'choices' => [
-                    'femme' => false,
-                    'homme' => false,
-                ],
-                'choice_label' => function ($choice, $key, $value) {
-                    if (true === $choice) {
-                        return 'Choisissez';
-                    }
-
-                    return strtoupper($key);
-
-                    // or if you want to translate some key
-                    //return 'form.choice.'.$key;
-                }
+                'placeholder' => 'Choose an option',
+                'required' => false,
             ])
-        # liste déroulante (h /f)
             ->add('city', TextType::class)
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
@@ -56,35 +46,61 @@ class UserController extends AbstractController
             ->add('submit', SubmitType::class)
             ->getForm();
 
+        # 3. Récupération des infos
         $form->handleRequest($request);
 
+        # 4. Si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
 
-            #3. Encodage du MDP
+            # 4a. On encode le MDP
             $user->setPassword(
                 $encoder->encodePassword($user, $user->getPassword())
             );
 
-            #4. Sauvegarde en BDD
+            # 4b. On gere l'Upload de l'image
+            /** @var UploadedFile $featuredImage */
+            $featuredImage = $form->get('featuredImage')->getData();
+
+            if ($featuredImage) {
+                $originalFilename = pathinfo($featuredImage->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$featuredImage->guessExtension();
+
+                try {
+                    $featuredImage->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'featureImagename' property to store the PDF file name
+                // instead of its contents
+
+                # on stock dans la BDD
+                $user->setFeaturedImage($newFilename);
+            }
+
+            # 4d. on sauvegarde en BDD
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
-            #5. Notification Flash
+            # 4e. Notification Flash
             $this->addFlash('notice', 'Merci pour votre inscription !');
 
-            #6. Redirection FIXME modifier l'url vers page connexion
+            # 4f. Redirection FIXME modifier l'url vers page connexion
             return $this->redirectToRoute('index');
 
         }
 
-        #  la Vue
+        #5. Transmission à la Vue
         return $this->render('user/create.html.twig', [
             'form' =>$form->createView()
         ]);
 
     }
-
 
 }
 
